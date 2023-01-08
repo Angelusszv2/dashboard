2c347e4df88c21dc815694b7ca1f7cfc1cb9dc15
Angelo <angelodelperez@gmail.com>
2023-01-08 00:06:53 -0500

    finish

---
+++ src/components/Persona.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useState} from 'react';
+import Personaje from "../images/image-jeremy.png"
+import "../styles/Persona.css"
+function Persona(props) {
+  const [daily,setDaily] = useState(false);
+  const [weekly,setWeekly] = useState(false);
+  const [monthly,setMonthly] = useState(false);
+  let cambio = (e)=>{
+    if (e.target.textContent === "Daily"){
+      setDaily(true);
+      setWeekly(false);
+      setMonthly(false);
+    }
+    if (e.target.textContent === "Weekly"){
+      setDaily(false);
+      setWeekly(true);
+      setMonthly(false);
+    }
+    if (e.target.textContent === "Monthly"){
+      setDaily(false);
+      setWeekly(false);
+      setMonthly(true);
+    }
+    props.cambio(e.target.textContent.toLowerCase())
+  }  
+    return(
+        <div className="persona" >
+          <div className="persona__info">
+            <div className='persona__foto'>
+              <img src={Personaje} alt = "Jeremy" />
+            </div>
+            <div className='persona-infodetalle'>
+              <p>Report for</p>
+              <p><span>{props.name}</span> <span>{props.lastname}</span></p>
+            </div>
+          </div>
+          <div className="persona__tiempo">
+            <div className={`tiempo__dia ${daily?"marcado":"nomarcado"}`} onClick={cambio} >Daily</div>
+            <div className={`tiempo__semana ${weekly?"marcado":"nomarcado"}` } onClick={cambio}>Weekly</div>
+            <div className={`tiempo__semana ${monthly?"marcado":"nomarcado"}` } onClick={cambio}>Monthly</div>
+          </div> 
+        </div>
+    )
+}
+
+export default Persona
